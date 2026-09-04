6fb77e172053a90a22838d5ff83ef53fd9c6cdc2
Kartoffelman363 <aljaz.roglic@iware.si>
2026-09-04 09:00:37 +0200

    Updated compile scripts

--- compiler_debug.ps1
+++ compiler_debug.ps1
@@ -1,3 +1,3 @@
-dotnet publish .\src\Compilers\Server\VBCSCompiler\AnyCpu\VBCSCompiler.csproj -c Debug -o .\artifacts\CompilerDebug /p:DebugType=portable /p:IncludeSymbols=true -f net9.0
-dotnet publish .\src\Compilers\CSharp\csc\AnyCpu\csc.csproj -c Debug -o .\artifacts\CompilerDebug /p:DebugType=portable /p:IncludeSymbols=true -f net9.0
-dotnet publish .\src\Compilers\VisualBasic\vbc\AnyCpu\vbc.csproj -c Debug -o .\artifacts\CompilerDebug /p:DebugType=portable /p:IncludeSymbols=true -f net9.0
+dotnet publish .\src\Compilers\Server\VBCSCompiler\AnyCpu\VBCSCompiler.csproj -c Debug -o .\artifacts\CompilerDebug /p:DebugType=portable /p:IncludeSymbols=true -f net10.0
+dotnet publish .\src\Compilers\CSharp\csc\AnyCpu\csc.csproj -c Debug -o .\artifacts\CompilerDebug /p:DebugType=portable /p:IncludeSymbols=true -f net10.0
+dotnet publish .\src\Compilers\VisualBasic\vbc\AnyCpu\vbc.csproj -c Debug -o .\artifacts\CompilerDebug /p:DebugType=portable /p:IncludeSymbols=true -f net10.0
--- compiler_release.ps1
+++ compiler_release.ps1
@@ -1,3 +1,3 @@
-dotnet publish .\src\Compilers\Server\VBCSCompiler\AnyCpu\VBCSCompiler.csproj -c Release -o .\artifacts\CompilerRelease /p:DebugType=portable /p:IncludeSymbols=true -f net9.0
-dotnet publish .\src\Compilers\CSharp\csc\AnyCpu\csc.csproj -c Release -o .\artifacts\CompilerRelease /p:DebugType=portable /p:IncludeSymbols=true -f net9.0
-dotnet publish .\src\Compilers\VisualBasic\vbc\AnyCpu\vbc.csproj -c Release -o .\artifacts\CompilerRelease /p:DebugType=portable /p:IncludeSymbols=true -f net9.0
+dotnet publish .\src\Compilers\Server\VBCSCompiler\AnyCpu\VBCSCompiler.csproj -c Release -o .\artifacts\CompilerRelease /p:DebugType=portable /p:IncludeSymbols=true -f net10.0
+dotnet publish .\src\Compilers\CSharp\csc\AnyCpu\csc.csproj -c Release -o .\artifacts\CompilerRelease /p:DebugType=portable /p:IncludeSymbols=true -f net10.0
+dotnet publish .\src\Compilers\VisualBasic\vbc\AnyCpu\vbc.csproj -c Release -o .\artifacts\CompilerRelease /p:DebugType=portable /p:IncludeSymbols=true -f net10.0
