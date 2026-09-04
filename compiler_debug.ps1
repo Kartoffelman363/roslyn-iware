023a562064e03fdd08924bf05443603b5d329ac7
Kartoffelman363 <aljaz.roglic@iware.si>
2026-09-04 11:09:02 +0200

    Added Microsoft.Build.Tasks.CodeAnalysis.csproj to compiler build scripts

--- compiler_debug.ps1
+++ compiler_debug.ps1
@@ -1,3 +1,7 @@
-dotnet publish .\src\Compilers\Server\VBCSCompiler\AnyCpu\VBCSCompiler.csproj -c Debug -o .\artifacts\CompilerDebug /p:DebugType=portable /p:IncludeSymbols=true -f net10.0
-dotnet publish .\src\Compilers\CSharp\csc\AnyCpu\csc.csproj -c Debug -o .\artifacts\CompilerDebug /p:DebugType=portable /p:IncludeSymbols=true -f net10.0
-dotnet publish .\src\Compilers\VisualBasic\vbc\AnyCpu\vbc.csproj -c Debug -o .\artifacts\CompilerDebug /p:DebugType=portable /p:IncludeSymbols=true -f net10.0
+$debug = ".\artifacts\CompilerDebug"
+$bincore = Join-Path $debug "bincore"
+
+dotnet publish .\src\Compilers\Core\MSBuildTask\MSBuild\Microsoft.Build.Tasks.CodeAnalysis.csproj -c Debug -o $debug /p:DebugType=portable /p:IncludeSymbols=true -f net10.0
+dotnet publish .\src\Compilers\Server\VBCSCompiler\AnyCpu\VBCSCompiler.csproj -c Debug -o $bincore /p:DebugType=portable /p:IncludeSymbols=true -f net10.0
+dotnet publish .\src\Compilers\CSharp\csc\AnyCpu\csc.csproj -c Debug -o $bincore /p:DebugType=portable /p:IncludeSymbols=true -f net10.0
+dotnet publish .\src\Compilers\VisualBasic\vbc\AnyCpu\vbc.csproj -c Debug -o $bincore /p:DebugType=portable /p:IncludeSymbols=true -f net10.0
--- compiler_release.ps1
+++ compiler_release.ps1
@@ -1,3 +1,7 @@
-dotnet publish .\src\Compilers\Server\VBCSCompiler\AnyCpu\VBCSCompiler.csproj -c Release -o .\artifacts\CompilerRelease /p:DebugType=portable /p:IncludeSymbols=true -f net10.0
-dotnet publish .\src\Compilers\CSharp\csc\AnyCpu\csc.csproj -c Release -o .\artifacts\CompilerRelease /p:DebugType=portable /p:IncludeSymbols=true -f net10.0
-dotnet publish .\src\Compilers\VisualBasic\vbc\AnyCpu\vbc.csproj -c Release -o .\artifacts\CompilerRelease /p:DebugType=portable /p:IncludeSymbols=true -f net10.0
+$release = ".\artifacts\CompilerRelease"
+$bincore = Join-Path $release "bincore"
+
+dotnet publish .\src\Compilers\Core\MSBuildTask\MSBuild\Microsoft.Build.Tasks.CodeAnalysis.csproj -c Release -o $release /p:DebugType=portable /p:IncludeSymbols=true -f net10.0
+dotnet publish .\src\Compilers\Server\VBCSCompiler\AnyCpu\VBCSCompiler.csproj -c Release -o $bincore /p:DebugType=portable /p:IncludeSymbols=true -f net10.0
+dotnet publish .\src\Compilers\CSharp\csc\AnyCpu\csc.csproj -c Release -o $bincore /p:DebugType=portable /p:IncludeSymbols=true -f net10.0
+dotnet publish .\src\Compilers\VisualBasic\vbc\AnyCpu\vbc.csproj -c Release -o $bincore /p:DebugType=portable /p:IncludeSymbols=true -f net10.0
